c01d73568a8744e643b45bc3ec8d87c9cb52e783
ryanwr <ryantwr@gmail.com>
2016-11-19 21:46:36 +0000

    Readded files, and gradle files

---
+++ src/main/java/com/ryanwelch/wordstore/WordStore.java
@@ -0,0 +1,9 @@
+package com.ryanwelch.wordstore;
+
+public interface WordStore {
+    void add(String word);
+
+    int count(String word);
+
+    void remove(String word);
+}
---
+++ src/main/java/com/ryanwelch/wordstore/WordStoreRedBlackTreeImp.java
@@ -0,0 +1,9 @@
+package com.ryanwelch.wordstore;
+
+/**
+ * Copyright 2016 (C) Ryan Welch
+ *
+ * @author Ryan Welch
+ */
+public class WordStoreRedBlackTreeImp {
+}
