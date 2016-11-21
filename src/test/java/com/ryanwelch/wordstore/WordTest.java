e1b63d83761421bcaec7842f1a41d0f0f2fce8e1
ryanwr <ryantwr@gmail.com>
2016-11-21 20:21:39 +0000

    Adds CSV output for all tests

--- src/test/java/com/ryanwelch/wordstore/WordTest.java
+++ src/test/java/com/ryanwelch/wordstore/WordTest.java
@@ -1,7 +1,6 @@
 package com.ryanwelch.wordstore;
 
 import java.io.FileWriter;
-import java.io.IOError;
 import java.io.IOException;
 import java.util.Scanner;
 
@@ -14,56 +13,86 @@ public class WordTest {
 
     public static void main(String[] args) throws IOException {
         Scanner input = new Scanner(System.in);
-        StringBuilder csvOutput = new StringBuilder();
         System.out.println("Enter seed: ");
         long seed = input.nextLong();
 
         WordStoreFactory.WordStoreType[] values = WordStoreFactory.WordStoreType.values();
         WordStoreFactory[] factories = new WordStoreFactory[WordStoreFactory.WordStoreType.values().length];
-        csvOutput.append("size");
+
+        StringBuilder addWordsCsvOutput = new StringBuilder();
+        StringBuilder addWordsNonEmptyCsvOutput = new StringBuilder();
+        StringBuilder countWordsCsvOutput = new StringBuilder();
+        StringBuilder removeWordsCsvOutput = new StringBuilder();
+        addWordsCsvOutput.append("size");
+        addWordsNonEmptyCsvOutput.append("size");
+        countWordsCsvOutput.append("size");
+        removeWordsCsvOutput.append("size");
         for(int i = 0; i < values.length; i++) {
             factories[i] = new WordStoreFactory(values[i]);
-            csvOutput.append(", ");
-            csvOutput.append(values[i].toString());
+            addWordsCsvOutput.append(", ").append(values[i].toString());
+            addWordsNonEmptyCsvOutput.append(", ").append(values[i].toString());
+            countWordsCsvOutput.append(", ").append(values[i].toString());
+            removeWordsCsvOutput.append(", ").append(values[i].toString());
         }
-        csvOutput.append("\n");
+        addWordsCsvOutput.append("\n");
+        addWordsNonEmptyCsvOutput.append("\n");
+        countWordsCsvOutput.append("\n");
+        removeWordsCsvOutput.append("\n");
 
-        int[] sizes = new int[]{1000, 100000};
+        int[] sizes = new int[]{1000, 100000, 500000, 1000000};
         long time;
 
         for(int size : sizes) {
             System.out.println("\n==================================\nTime taken for size of " + size + "\n==================================");
 
             System.out.println("\nAdd words to empty store");
-            csvOutput.append(size);
+            addWordsCsvOutput.append(size);
             for(WordStoreFactory factory : factories) {
-                time = addWordsToEmpty(factory.get(size), new WordGen(seed), size) / 1000000;
-                System.out.println(String.format("%1$-20s", factory.getType().toString()) + time + "ms");
-                csvOutput.append(", ").append(time);
+                time = addWordsToEmpty(factory.get(size), new WordGen(seed), size) / 1000;
+                System.out.println(String.format("%1$-20s", factory.getType().toString()) + time + "us");
+                addWordsCsvOutput.append(", ").append(time);
             }
-            csvOutput.append("\n");
+            addWordsCsvOutput.append("\n");
 
             System.out.println("\nAdd words non empty store");
+            addWordsNonEmptyCsvOutput.append(size);
             for(WordStoreFactory factory : factories) {
-                time = addWords(factory.get(size), new WordGen(seed), size, 10000) / 1000000;
-                System.out.println(String.format("%1$-20s", factory.getType().toString()) + time + "ms");
+                time = addWords(factory.get(size), new WordGen(seed), size, 10000) / 1000;
+                System.out.println(String.format("%1$-20s", factory.getType().toString()) + time + "us");
+                addWordsNonEmptyCsvOutput.append(", ").append(time);
             }
+            addWordsNonEmptyCsvOutput.append("\n");
 
             System.out.println("\nCount words in store");
+            countWordsCsvOutput.append(size);
             for(WordStoreFactory factory : factories) {
-                time = countWords(factory.get(size), new WordGen(seed), size, 10000) / 1000000;
-                System.out.println(String.format("%1$-20s", factory.getType().toString()) + time + "ms");
+                time = countWords(factory.get(size), new WordGen(seed), size, 10000) / 1000;
+                System.out.println(String.format("%1$-20s", factory.getType().toString()) + time + "us");
+                countWordsCsvOutput.append(", ").append(time);
             }
+            countWordsCsvOutput.append("\n");
 
             System.out.println("\nRemove words from store");
+            removeWordsCsvOutput.append(size);
             for(WordStoreFactory factory : factories) {
-                time = removeWords(factory.get(size), new WordGen(seed), size, 10000) / 1000000;
-                System.out.println(String.format("%1$-20s", factory.getType().toString()) + time + "ms");
+                time = removeWords(factory.get(size), new WordGen(seed), size, 10000) / 1000;
+                System.out.println(String.format("%1$-20s", factory.getType().toString()) + time + "us");
+                removeWordsCsvOutput.append(", ").append(time);
             }
+            removeWordsCsvOutput.append("\n");
         }
 
-        FileWriter writer = new FileWriter("test.csv");
-        writer.write(csvOutput.toString());
+        FileWriter writer = new FileWriter("docs/addwords.csv");
+        writer.write(addWordsCsvOutput.toString());
+        writer.close();
+        writer = new FileWriter("docs/addwordsnonempty.csv");
+        writer.write(addWordsNonEmptyCsvOutput.toString());
+        writer.close();
+        writer = new FileWriter("docs/countwords.csv");
+        writer.write(countWordsCsvOutput.toString());
+        writer.close();
+        writer = new FileWriter("docs/removewords.csv");
+        writer.write(removeWordsCsvOutput.toString());
         writer.close();
 
     }
